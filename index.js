5e061ab8f0e79dad3111c14bbef3683cbc7922d5
merazzak <info.merazzak@gmail.com>
2025-03-29 16:06:59 +0600

    update env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -7,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://surerMaster:fV3uoXHazGMSItMY@surer-tori.rjlduxb.mongodb.net/?retryWrites=true&w=majority&appName=Surer-Tori`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@surer-tori.rjlduxb.mongodb.net/?retryWrites=true&w=majority&appName=Surer-Tori`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
