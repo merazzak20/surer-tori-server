9cb03e49fccfd9ac1f40127b9ee5cef59ee06ec5
merazzak <info.merazzak@gmail.com>
2025-03-29 17:10:42 +0600

    crete feedback get route

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/feedback", async (req, res) => {
+      const result = await feedbackCollection.find().toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
